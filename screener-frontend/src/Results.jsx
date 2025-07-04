a002b2387f4966d6ba5cb2f056d15d33cb410b14
Earth Walker <pineconefirestarter@pm.me>
2025-07-04 14:02:23 -0400

    implement basic version of traffic light results section, given static result data in the shape used by the previous version of the screener

--- screener-frontend/src/Results.jsx
+++ screener-frontend/src/Results.jsx
@@ -1,35 +1,128 @@
-import { For } from "solid-js";
+import { For, Index, createSignal, Show } from "solid-js";
 import LoopCard from "./LoopCard";
 
 export default function Results({ results }) {
+  const [testResults, setTestResults] = createSignal({});
+  setTestResults({
+    benefits: {
+      phlLoop: {
+        eligibility: {
+          result: false,
+          checks: {
+            tenOrMoreYearsOwnerOccupant: null,
+            notEnrolledInPhlLoop: null,
+            noTenYearTaxAbatement: null,
+            loopTaxAssessmentEligible: null,
+            phillyOwnerOccupantHomeowner: false,
+            underPreviousAnnualGrossIncome: null,
+            notTaxDelinquentOrIsEnrolledInOopaEligible: null,
+          },
+        },
+      },
+      phlSeniorCitizenTaxFreeze: {
+        eligibility: {
+          result: false,
+          checks: {
+            notEnrolledInPhlSeniorCitizenTaxFreeze: null,
+            meetsPhlSeniorCitizenTaxFreezeAgeRequirements: null,
+            phillyOwnerOccupantHomeowner: false,
+            underMaritalStatusBasedCurrentMonthlyGrossIncome: null,
+          },
+        },
+      },
+      phlLowIncomeTaxFreeze: {
+        eligibility: {
+          result: false,
+          checks: {
+            phillyOwnerOccupantHomeowner: false,
+            notEnrolledInPhlLowIncomeTaxFreeze: null,
+            underMaritalStatusBasedCurrentMonthlyGrossIncome: null,
+          },
+        },
+      },
+      phlOopa: {
+        eligibility: {
+          result: false,
+          checks: {
+            taxDelinquent: null,
+            phillyOwnerOccupantHomeowner: false,
+            notEnrolledInPhlOopa: null,
+          },
+        },
+      },
+      phlHomesteadExemption: {
+        eligibility: {
+          result: false,
+          checks: {
+            noTenYearTaxAbatement: null,
+            phillyOwnerOccupantHomeowner: false,
+            notEnrolledInPhlHomesteadExemption: null,
+          },
+        },
+      },
+    },
+  });
   return (
     <>
       <div class="my-4 mx-8 p-4 border border-gray-300 rounded shadow-sm">
         <h2 class="text-gray-800 text-md font-bold">Results</h2>
         <div class="my-2">
-          {results() && (
-            <For each={results()}>
-              {(item) =>
-                item && (
-                  <div class="border-t border-gray-300 p-2 ">
-                    <div class="flex flex-col">
-                      <For each={Object.entries(item)}>
-                        {([key, value]) => (
-                          <div class="flex text-sm text-gray-700">
-                            <span class="font-medium capitalize">{key}:</span>
-                            <span>{value?.toString()}</span>
-                          </div>
-                        )}
-                      </For>
-                    </div>
-                  </div>
-                )
-              }
-            </For>
+          {testResults() && (
+            <Index each={Object.keys(testResults()["benefits"])}>
+              {(benefitName, index) => (
+                <>
+                  <h3 class="font-bold mt-4">
+                    <span>{benefitName()}</span>
+                    <span>
+                      {" "}
+                      -{" "}
+                      <Show
+                        when={
+                          testResults()["benefits"][benefitName()][
+                            "eligibility"
+                          ]["result"] !== null
+                        }
+                        fallback={"null"}
+                      >
+                        {testResults()["benefits"][benefitName()][
+                          "eligibility"
+                        ]["result"].toString()}
+                      </Show>
+                    </span>
+                  </h3>
+                  <Index
+                    each={Object.keys(
+                      testResults()["benefits"][benefitName()]["eligibility"][
+                        "checks"
+                      ]
+                    )}
+                  >
+                    {(checkName, index) => (
+                      <>
+                        <p>
+                          {checkName()} -{" "}
+                          <Show
+                            when={
+                              testResults()["benefits"][benefitName()][
+                                "eligibility"
+                              ]["checks"][checkName()] !== null
+                            }
+                            fallback={"null"}
+                          >
+                            {testResults()["benefits"][benefitName()][
+                              "eligibility"
+                            ]["checks"][checkName()].toString()}
+                          </Show>
+                        </p>
+                      </>
+                    )}
+                  </Index>
+                </>
+              )}
+            </Index>
           )}
         </div>
       </div>
-      <LoopCard />
     </>
   );
 }
