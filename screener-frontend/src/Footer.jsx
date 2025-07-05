0e4234b4e736210ef1cbf41ca3dcfcbfb398af03
Earth Walker <pineconefirestarter@pm.me>
2025-07-05 12:52:52 -0400

    add header and footer components to the screener page and adjust spacing around screener

---
+++ screener-frontend/src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer class="mx-8 my-8 text-center">
+      <p>Organization Name</p>
+    </footer>
+  );
+}
---
+++ screener-frontend/src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header class="bg-sky-600 px-8 py-8">
+      <h1 class="text-4xl font-bold text-white">Organization Name</h1>
+    </header>
+  );
+}
