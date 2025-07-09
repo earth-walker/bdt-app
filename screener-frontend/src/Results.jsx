759d3a63edf16f239834f670cc1c96d8950ed79a
Earth Walker <pineconefirestarter@pm.me>
2025-07-09 11:51:05 -0400

    fully restore results component to original state

--- screener-frontend/src/Results.jsx
+++ screener-frontend/src/Results.jsx
@@ -1,11 +1,10 @@
-import { For, createSignal } from "solid-js";
-import EligibilityResults from "./EligibilityResults";
+import { For } from "solid-js";
 
 export default function Results({ results }) {
   return (
-    <>
-      <div class="my-4 mx-8 p-4 border border-gray-300 rounded shadow-sm">
-        <h2 class="text-gray-800 text-md font-bold">Results</h2>
+    <div class="my-4 mx-8 p-4 border border-gray-300 rounded shadow-sm">
+      <h2 class="text-gray-800 text-md font-bold">Results</h2>
+      <div class="my-2">
         {results() && results().inputs && (
           <>
             <h2 class="text-gray-600 text-sm font-bold">Inputs</h2>
@@ -34,6 +33,6 @@ export default function Results({ results }) {
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
