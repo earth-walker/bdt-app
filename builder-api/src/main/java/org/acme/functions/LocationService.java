c84cd098217ca502292eb99e6fd5dc2a4b561814
Michael Dratch <michael.s.dratch@gmail.com>
2025-07-29 20:51:06 -0400

    add location service

---
+++ builder-api/src/main/java/org/acme/functions/LocationService.java
@@ -0,0 +1,86 @@
+package org.acme.functions;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.Unremovable;
+import io.agroal.api.AgroalDataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Unremovable
+@ApplicationScoped
+public class LocationService {
+
+    @Inject
+    AgroalDataSource dataSource;
+
+    public Connection getDbConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static List<String> lookup(String column, Map<String, Object> filters) {
+        List<String> results = new ArrayList<>();
+        StringBuilder sql = new StringBuilder("SELECT DISTINCT ").append(column).append(" FROM locations WHERE ");
+
+        // construct WHERE clause; assume everything is ANDed together
+        boolean first = true;
+        for (String key : filters.keySet()) {
+            if (!first) {
+                sql.append(" AND ");
+            }
+            sql.append(key).append(" = ?");
+            first = false;
+        }
+
+        LocationService service = Arc.container().instance(LocationService.class).get();
+
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            connection = service.getDbConnection();
+            pstmt = connection.prepareStatement(sql.toString());
+
+            // Set the values dynamically
+            int index = 1;
+            for (Object value : filters.values()) {
+                String stringValue = value.toString(); // db only has strings
+                pstmt.setString(index++, stringValue);
+            }
+
+            rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                String thisString = rs.getString(column);
+                results.add(thisString);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return results;
+
+    }
+}
--- builder-api/src/main/java/org/acme/service/DmnService.java
+++ builder-api/src/main/java/org/acme/service/DmnService.java
@@ -3,7 +3,7 @@ import org.acme.model.domain.Screener;
 import java.io.IOException;
 import java.util.Map;
 
-public interface DmnEvaluationService {
+public interface DmnService {
     public Map<String, Object> evaluateDecision(Screener screener, Map<String, Object> inputs) throws IOException;
     public String compilePublishedDmnModel(String screenerId) throws Exception;
     public void compileWorkingDmnModel(Screener screener) throws Exception;
